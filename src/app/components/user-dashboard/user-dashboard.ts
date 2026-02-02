90a28524bf49366cfe28754ad93d9ab6f6d65982
Kevin Y. <youngkevin7447@gmail.com>
2026-02-01 21:00:35 -0800

    Update HTML and CSS for expense tracker UI

--- src/app/components/user-dashboard/user-dashboard.ts
+++ src/app/components/user-dashboard/user-dashboard.ts
@@ -1,28 +1,29 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgOptimizedImage } from '@angular/common';
 import { UserSpendingDataService } from '../../services/user-spending-data';
 import { UserSpending } from '../../models/user-spending';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CurrencyPipe, DatePipe, NgOptimizedImage],
   templateUrl: './user-dashboard.html',
   styleUrl: './user-dashboard.css',
 })
-export class UserDashboardComponent implements OnInit{
+export class UserDashboardComponent implements OnInit {
 
-  userSpending ?: UserSpending;
+  userSpending?: UserSpending;
+  today: Date = new Date();
 
   constructor(
     private userSpendingDataService: UserSpendingDataService,
-    private cdr: ChangeDetectorRef) {}
+    private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.userSpendingDataService.getUserSpending().subscribe({
       next: data => {
-        this.userSpending = data,
-        this.cdr.detectChanges()  // Force template to update.
+        this.userSpending = data;
+        this.cdr.detectChanges();  // Force template to update.
       },
       error: err => console.error('Error in user-dashboard component:', err)
     });
