40a42595ffa5e0cd8caacbab627b3dd975321e29
Kevin Y. <youngkevin7447@gmail.com>
2026-02-02 01:32:10 -0800

    feat: include key field in Card model to track object keys

--- src/app/services/user-spending-data.ts
+++ src/app/services/user-spending-data.ts
@@ -23,16 +23,19 @@ export class UserSpendingDataService {
           imageUrl: string;
         }
 
-        // Convert the 'cards' objects into an array of Card instances safely
-        const cards: Card[] = Object.values(data.cards)
-          .filter(
-            (c): c is CardData =>
-              typeof c === 'object' &&
-              c !== null &&
-              'name' in c &&
-              'spending' in c
-          )
-          .map(c => new Card(c.name, c.spending, c.limit, c.imageUrl));
+        // Convert the 'cards' objects into an array of Card instances safely.
+        const cards: Card[] = Object.entries(data.cards)
+          .filter((entry): entry is [string, CardData] => {
+            const [key, card_data] = entry;  // Destructure entry.
+            return (
+              typeof card_data === 'object' &&
+              card_data !== null &&
+              'name' in card_data &&
+              'spending' in card_data
+            );
+          })
+          .map(([key, card_data]) => new Card(key, card_data.name,
+            card_data.spending, card_data.limit, card_data.imageUrl));
 
         return new UserSpending(
           data.spending_ytd,
